b2b96117f0862896edee9bc4276bc028b4c4efdc
Yuu <yuuhakobe1200@gmail.com>
2021-11-25 20:41:46 +0200

    add sum(22) and README.md

---
+++ sum(22).py
@@ -0,0 +1,32 @@
+import os
+
+# Attīra termināli, priekš linuxiem likt 
+if os.name == "nt": # posx, java, nt
+    os.system("cls")
+else:
+    os.system("clear")
+
+# Pziņo, kā iziet no cikla
+print("Iziet no programmas var ar kombināciju (Ctrl + C)")
+
+# Liek atkārtoties kodam bezgalīgi
+while True:
+    # Prasa ciparu. Sagaida, ka ievadīs: type int
+    number = int(input("Ievadiet pozitīvu divciparu skaitli -> "))
+
+    # Iegūst simbolu skaitu iekš mainīgā number
+    num = len(number)
+
+    # Pārbauda vai ir mainīgajā ir 2 simboli
+    if num != 2:
+        print("Ir jāievada divciparu skaitlis")
+    else:
+        print(f"Jūs ievadijāt {number}")
+
+        ''' Izveido mainīgo int d(digit).
+            Katrā iterācijā d == number[d+1] tad
+            to konverto uz list'''
+        result = [int(d) for d in str(number)]
+        
+        # Summē 2 ievadītos skaitļus un izvada 
+        print(result[0] + result[1])
