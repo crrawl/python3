2c98b5afe5e7fdfe0d7a2ef9a34537133a5dacf7
Yuu <yuuhakobe1200@gmail.com>
2021-11-27 11:04:22 +0200

    update

--- sum(22).py
+++ sum(22).py
@@ -1,13 +1,16 @@
 import os
 
-# Attīra termināli, priekš linuxiem likt 
+''' 
+Pārbauda, kāda operētājsistēma ir lietotājam
+skatoties no rezultāta izvadas attiecīga komanda
+''' 
 if os.name == "nt": # posx, java, nt
     os.system("cls")
 else:
     os.system("clear")
 
 # Pziņo, kā iziet no cikla
-print("Iziet no programmas var ar kombināciju (Ctrl + C)")
+print(">> Iziet no programmas var ar kombināciju (Ctrl + C)")
 
 # Liek atkārtoties kodam bezgalīgi
 while True:
