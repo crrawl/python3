9de1df1ad537f1a3f929169ccaa876b030d30ae0
Yuu <yuuhakobe1200@gmail.com>
2021-12-02 16:20:24 +0200

    add a start

--- coding_test.py
+++ coding_test.py
@@ -1,1 +1,8 @@
-print("Hello, Wolrd")
+import os
+from pywebio.input import input
+from pywebio.output import put_text as printt
+
+name = input("username: ")
+age  = input("age: ")
+
+printt(f"Your name is {name} \nAnd your age is {age} ")
