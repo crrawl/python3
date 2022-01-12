22b9d0eb91913412ff76ef035e2b459b093eb1c9
Yuu <yuuhakobe1200@gmail.com>
2022-01-12 02:04:39 +0200

    add % oper

--- one_line_cauculator/main.py
+++ one_line_cauculator/main.py
@@ -27,6 +27,7 @@ while True:
         print(firstNummber * secondNummber)
     elif operator == "/":
         print(firstNummber / secondNummber)
-
+    elif operator == "%":
+        print(firstNummber % secondNummber)
     # all code upper can be writen with one line under.
     # print(eval(input()))
