e69949fc1d2a64983d4787af09494c7280fc0fb0
yukurasan <yuukuramu@gmail.com>
2022-03-24 22:09:12 +0200

    add get the question algortithm | v0.03

--- survey_houses/main.py
+++ survey_houses/main.py
@@ -9,6 +9,7 @@
 import os
 import random
 import json
+from time import sleep
 
 from rich import emoji
 from rich.console import Console
@@ -40,8 +41,6 @@ with open("questions.json") as dict:
 # shortcut
 console = Console(theme=custom_theme)
 
-# console.print( questions['N1'] )
-
 ### FUNCTIONS
 def clearTerminal():
     """
@@ -54,8 +53,18 @@ def clearTerminal():
         os.system("clear")
 
 
+def get_the_question():
+    """
+    Return a question N1 - N*
+    """
+    
+    for n in questions.keys():
+        console.print(questions[n])
+
 
 # copyright
+# TODO uztaisit timeout 2sec
+clearTerminal()
 print(
     """
 ----------------------------------------------------- 
@@ -67,8 +76,13 @@ print(
 -----------------------------------------------------
 """
 )
-
-console.print("[attention][!][/] lūdzu palaižat šo skriptu konsolē kura atbalsta UTF-8")
+console.print("[attention][!][/] Lūdzu palaižat šo skriptu konsolē kura atbalsta UTF-8\n")
+console.print("[attention][!][/] Notiek ielāde ...")
+# Sleep 2 sec! 
+sleep(2)
+console.print("[success][+][/] Ielāde veiksmīga!")
+sleep(0.5)
+clearTerminal()
 
 # TODO
 """
@@ -78,21 +92,31 @@ console.print("[attention][!][/] lūdzu palaižat šo skriptu konsolē kura atba
 ### PROGRAMMA
 try:
     while True:
+
         console.print("[option][?][/] Ievadiet mājas, dzīvokļa nummuru un cik piedalās aptaujā no dzīvokļa \nPiemērs: 2, 3, 2")
+        
         try:
             house = [int(n) for n in input("=> ").split(',')]
             console.print("[option][?][/] Cik no dzīvokļa piedalās aptaujā")
             
             # for i in range(house[2]):
+            get_the_question()
                 
-
         except ValueError:
+            clearTerminal()
             console.print("[error][-][/] Atļauti tikai cipari")
             continue
 
         print(house)
-        console.print(f"""[success][+][/] Jūsu mājas nummurs: {house[0]}, dzīvokļa nummurs: {house[1]}""")
+        try:
+            console.print(f"""[success][+][/] Jūsu mājas nummurs: {house[0]}, dzīvokļa nummurs: {house[1]}, Aptaujājamo dzīvoklī: {house[2]}""")
+        except IndexError:
+            clearTerminal()
+            console.print("[error][-][/] Nekorekta ievade! \n[error]Piemērs[/] - 2, 2, 2: Mājas nummurs: 2, Dzīvokļa nummurs: 2, Aptaujājamo dzīvoklī: 2")
+            continue
+
         
+
         break
 except KeyboardInterrupt:
 
