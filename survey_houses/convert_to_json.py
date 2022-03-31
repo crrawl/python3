a2c44881f4ef90c7f931f5aa21b597838e16d4a9
yukurasan <yuukuramu@gmail.com>
2022-03-31 11:50:19 +0300

    Update question list

--- survey_houses/convert_to_json.py
+++ survey_houses/convert_to_json.py
@@ -1,13 +1,13 @@
 import json
 	
 # Data to be written
-dictionary ={
-    "N1": """\n[option](1.)[/] Vai jūs esat ar mieru, ka mūsu māju kontrolēs gudrs AI - Altron ?
-[white]-[/] [error](Nē)[/], [white]/[/] [warning](Patreiz neitrāls)[/], [white]+[/] [success]Jā![/], \n""",
-    "N2": """\n[option](2.)[/]  - SUKKAAA ?
-[white]-[/] [error](Nē)[/], [white]/[/] [warning](Patreiz neitrāls)[/], [white]+[/] [success]Jā![/], \n""",
-    "N3": """\n[option](3.)[/]  - SUKKAAA 3333333 ?
-[white]-[/] [error](Nē)[/], [white]/[/] [warning](Patreiz neitrāls)[/], [white]+[/] [success]Jā![/], \n"""
+dictionary = {
+    "N1": """\n[option](1.)[/] Vai jūs esat ar mieru, ka mūsu māju kontrolēs gudrs AI - Altron ?\n""",
+    "N2": """\n[option](2.)[/]  - Vai jūs apmierināti par saviem kainiņiem ? """,
+    "N3": """\n[option](3.)[/]  - Vai vajag renovēt pagalma ielas ? """,
+    "N4": """\n[option](4.)[/]  - Vai vajag aizliegt sētā iebraukt jebkurai mašīnai kuras logo nav TESLA ? """,
+    "N5": """\n[option](5.)[/]  - Vai ierobežot laiku, kad bērni pagalmā var klausīties mūziku: Tikai 20:00 - 5:00 """,
+    "N6": """\n[option](6.)[/]  - Vai vajag uzstādīt jaunu nodokli uz iebraukšanu pagalmā. Protams par visiem ienakumiem es nopirkšu jaunu TESLA, jo savādāk, kā es iebraukšu pagalmā. """
 }
 	
 json_object = json.dumps(dictionary, indent = 4)
